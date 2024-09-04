b76e591fd2591fb12cdafed934b1e84824e01cee
Rafael Cadena <rafael@roadmaps.com.br>
2024-09-04 02:10:19 -0300

    feat: adding base controller and improving routes

---
+++ app/controllers/api/v1/base_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  module V1
+    class BaseController < ApplicationController
+      respond_to :json
+      protect_from_forgery with: :null_session, only: proc { |c| c.request.format.json? }
+      before_action :authenticate_user!
+
+      def authorize_admin
+        return nil if current_user.blank?
+
+        render json: {}, status: :unauthorized unless current_user.admin?
+      end
+
+      def authorize_employee
+        return nil if current_user.blank?
+
+        render json: {}, status: :unauthorized unless current_user.employee?
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,41 @@
 
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  resources :companies
+
+  namespace :api, defaults: { format: :json }, path: '/api' do
+    namespace :v1 do
+      post :webhooks, to: 'baas#webhooks'
+
+      resources :statements, only: %i[index update] do
+        member do
+          patch :archive
+        end
+      end
+      resources :users, only: %i[index create update] do
+        collection do
+          post :create_employee
+          post :create_admin
+        end
+        member do
+          patch :update_employee
+          delete :destroy_employee
+        end
+      end
+      resources :companies
+      resources :categories, only: %i[index create update destroy]
+      resources :cards, only: %i[index create update destroy]
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+
+  # Front routes
+  get 'app', to: 'home#index'
+  get 'app/*other', to: 'home#index'
+  # get 'app/cards', to: 'home#index'
+  # get 'app/categories', to: 'home#index'
+  # get 'app/users', to: 'home#index'
+  # get 'app/sign-in', to: 'home#index'
+  # get 'app/sign-up', to: 'home#index'
 end
