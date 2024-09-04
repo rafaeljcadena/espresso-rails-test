958be811406e791b7848bd022c6b533d2c380131
Rafael Cadena <rafael@roadmaps.com.br>
2024-09-04 02:01:38 -0300

    fix: adjusting user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,11 @@ class User < ApplicationRecord
   include DeviseTokenAuth::Concerns::User
 
   belongs_to :company
+  validates_associated :company
+  accepts_nested_attributes_for :company
+
   enum role: { admin: 0, employee: 1 }
 
   validates :role, presence: true
+  validates :email, uniqueness: true
 end
