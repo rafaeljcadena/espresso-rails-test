129906d5f2b4985017020fc2d34bbfcfc4fc2d6b
Rafael Cadena <rafael@roadmaps.com.br>
2024-09-04 02:03:18 -0300

    feat: adding user mailer

---
+++ spec/mailers/previews/user_preview.rb
@@ -0,0 +1,7 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user
+class UserPreview < ActionMailer::Preview
+
+  def welcome
+    UserMailer.welcome(User.last.id).deliver_now
+  end
+end
