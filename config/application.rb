9599e112ad9fe1cf681759b7177f3d67450f9d87
Rafael Cadena <rafael@roadmaps.com.br>
2024-09-24 19:57:31 -0300

    feat: adjusting environments for translation and mailer tests

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module EspressoRailsTest
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.i18n.available_locales = %i[en pt-BR]
+    config.i18n.default_locale = :'pt-BR'
   end
 end
