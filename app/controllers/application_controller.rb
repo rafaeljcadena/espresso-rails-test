cd8866a76026777d53b1652292d10a8ea505db39
Rafael Cadena <rafael@roadmaps.com.br>
2024-09-24 19:53:10 -0300

    fix: removing unused devise method from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,4 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
   include DeviseTokenAuth::Concerns::SetUserByToken
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[company_id role name])
-  end
 end
