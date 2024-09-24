43bf6640de1d22aee804b5f34c1c5fc1c8250833
Rafael Cadena <rafael@roadmaps.com.br>
2024-09-24 19:55:29 -0300

    refactor: adding text to I18n

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ApplicationMailer
   def welcome(user_id)
     @user = User.find(user_id)
 
-    mail to: @user.email, subject: 'Bem-vindo ao Espresso! Aqui estão suas credenciais de acesso'
+    mail to: @user.email, subject: I18n.t('mailers.welcome.subject')
   end
 end
