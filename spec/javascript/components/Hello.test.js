6f8fddabfe1509beeea1c850f3b95756d72bdce7
Rafael Cadena <rafael@roadmaps.com.br>
2024-09-24 20:09:14 -0300

    fix: removing unused test files

--- spec/javascript/components/Hello.test.js
+++
@@ -1,8 +0,0 @@
-import Hello from "../../../app/javascript/components/Hello";
-import {render} from "@testing-library/react";
-import "@testing-library/jest-dom";
-
-it('greets candidate name', () => {
-    const component = render(<Hello candidate={"John"}/>)
-    expect(component.getByText('Olá, John!')).toBeInTheDocument();
-});
